7b44fa1e76e21758a0891bcccf29ba9522b80c9d
Paul Kamp <pauljonaskamp@gmx.de>
2024-01-08 13:28:48 +0100

    firestore connection & update css varibales

---
+++ src/js/main.js
@@ -0,0 +1,29 @@
+import { initializeApp  } from "firebase/app";
+import { getFirestore, getDocs, collection } from "firebase/firestore";
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDBdJ44O8u9YvVDhw4gJZjenKb52PDh_V4",
+  authDomain: "park-bank-app.firebaseapp.com",
+  projectId: "park-bank-app",
+  storageBucket: "park-bank-app.appspot.com",
+  messagingSenderId: "678823168030",
+  appId: "1:678823168030:web:9f7da9355bd2587896460f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Cloud Firestore and get a reference to the service
+const db = getFirestore(app);
+
+export async function getBenchData() {
+    const querySnapshot = await getDocs(collection(db, "benches"));
+    querySnapshot.forEach((doc) => {
+        // doc.data() is never undefined for query doc snapshots
+        console.log(doc.id, " => ", doc.data());
+    });
+}
+
+//getBenchData();
