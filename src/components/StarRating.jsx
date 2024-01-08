3e4abfa65743cdc3c0537816478752efec1bbb4f
Tim <timmobello@gmail.com>
2024-01-08 18:55:51 +0100

    StarRating logic without svg

--- src/components/StarRating.jsx
+++ src/components/StarRating.jsx
@@ -1,1 +1,31 @@
+// src/StarRating.js
 import React, { useState } from 'react';
+import '../style/StarRating.css';
+
+const StarRating = () => {
+  const [rating, setRating] = useState(0);
+
+  const handleClick = (index) => {
+    setRating(index + 1);
+    // Add your desired functionality here
+    console.log("Selected rating:", rating);
+  };
+
+  return (
+    <div className="star-rating">
+      {Array(5)
+        .fill()
+        .map((_, index) => (
+          <span
+            key={index}
+            className={index < rating ? 'filled' : ''}
+            onClick={() => handleClick(index)}
+          >
+            ★
+          </span>
+        ))}
+    </div>
+  );
+};
+
+export default StarRating;
