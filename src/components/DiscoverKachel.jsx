5a05fc25104ebad8bfba3fbd3efe9df82ff5e015
Paul Kamp <pauljonaskamp@gmx.de>
2024-01-09 22:59:37 +0100

    Save Button Discover

--- src/components/DiscoverKachel.jsx
+++ src/components/DiscoverKachel.jsx
@@ -7,6 +7,15 @@ import CleanRating from "./CleanRating";
 
 function DiscoverKachel() {
     const [data, setData] = useState([]);
+    const [filledStates, setFilledStates] = useState([]);
+
+    const handleClick = (index) => {
+        setFilledStates(prevStates => {
+            const newStates = [...prevStates];
+            newStates[index] = !newStates[index];
+            return newStates;
+        });
+    };
 
     useEffect(() => {
         // Funktion zum Abrufen von Daten aus Firestore
@@ -16,6 +25,7 @@ function DiscoverKachel() {
                 const userData = dummyData;
                 console.log(userData)
                 setData(userData);
+                setFilledStates(Array(userData.length).fill(false));
             } catch (error) {
                 console.error('Fehler beim Abrufen der Daten: ', error);
             }
@@ -27,7 +37,7 @@ function DiscoverKachel() {
 
     return (
         <div className="discover-container">
-            {data.map((item) => (
+            {data.map((item, index) => (
                 <div key={item.id} className="data-container">
                     <div>
                         <div className="discover-img-container">
@@ -40,7 +50,12 @@ function DiscoverKachel() {
 
                         <div className='icon-holder'>
                             <img src="Assets/on-map.svg" alt="" />
-                            <img src="Assets/save.svg" alt="" />
+
+                            <svg xmlns="http://www.w3.org/2000/svg" onClick={() => handleClick(index)} width="19.346" height="25.157" viewBox="0 0 19.346 25.157">
+                                <g id="save-remove-svgrepo-com" transform="translate(-0.85 -4.73)">
+                                    <path id="Pfad_8"  fill={ filledStates[index] ? 'black' : 'none' } data-name="Pfad 8" d="M19.046,10.284V26.373c0,2.054-1.266,2.917-2.813,1.926l-4.785-3.1a1.875,1.875,0,0,0-1.851,0L4.813,28.3C3.266,29.29,2,28.426,2,26.373V10.284c0-2.422,1.7-4.4,3.787-4.4H15.26C17.342,5.88,19.046,7.863,19.046,10.284Z" stroke="#000" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2.3"/>
+                                </g>
+                            </svg>
                         </div>
 
                     </div>
