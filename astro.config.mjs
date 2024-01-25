3ad5c21d6be607798e3bbc9cb3a4c213dc782c16
Paul Kamp <pauljonaskamp@gmx.de>
2024-01-25 10:37:47 +0100

    adding image pfad

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-  server: { port: 1234, host: true}
+  server: { port: 1234, host: true},
+  output: 'static',
 });
